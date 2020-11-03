e60dd627055658f8c63f2863c270bb172848a518
GingerBeardx <ecryptographer@gmail.com>
2020-11-03 06:50:26 -0700

    Added Oracle and SQLConnection classes.

---
+++ Database/OracleConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    class OracleConnection : DBConnection
+    {
+        public OracleConnection(string connectionString) : base(connectionString)
+        {
+
+        }
+
+
+        public override void CloseConnection()
+        {
+            Console.WriteLine("Closed Oracle Connection");
+        }
+
+        public override void OpenConnection()
+        {
+            Console.WriteLine("Opened Oracle Connection");
+        }
+    }
+}
---
+++ Database/SqlConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Database
+{
+    public class SqlConnection : DBConnection
+    {
+        public SqlConnection(string connectionString) : base(connectionString)
+        {
+
+        }
+
+
+        public override void CloseConnection()
+        {
+            Console.WriteLine("Closed SQL Connection");
+        }
+
+        public override void OpenConnection()
+        {
+            Console.WriteLine("Opened SQL Connection");
+        }
+    }
+}

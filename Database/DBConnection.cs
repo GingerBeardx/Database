5cdd8bd9ab4ae20ecab0b6b9b687ef7619933807
GingerBeardx <ecryptographer@gmail.com>
2020-11-03 06:37:22 -0700

    Created DBConnection Class.

---
+++ Database/DBConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    public abstract class DBConnection
+    {
+        public string ConnectionString { get; set; }
+        public TimeSpan Timeout { get; set; }
+
+        public DBConnection(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new InvalidOperationException("The connection string cannot be null or empty.");
+
+            ConnectionString = connectionString;
+        }
+
+        public abstract void OpenConnection();
+        public abstract void CloseConnection();
+    }
+}

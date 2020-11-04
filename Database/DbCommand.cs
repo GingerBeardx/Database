62e1ea8f97c14371319b5ab0b619d855cd45231c
GingerBeardx <ecryptographer@gmail.com>
2020-11-03 18:27:47 -0700

    Added Command class for executing commands to the DB.

---
+++ Database/DbCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    class DbCommand
+    {
+        public DBConnection Connection { get; set; }
+        public string DBCommand { get; set; }
+
+        public DbCommand(DBConnection connection, string dbCommand)
+        {
+            if (connection == null)
+                throw new InvalidOperationException("A connection must be passed.");
+
+            if (string.IsNullOrWhiteSpace(dbCommand))
+                throw new InvalidOperationException("The command cannot be null or empty.");
+
+            Connection = connection;
+            DBCommand = dbCommand;
+        }
+
+        public void Execute()
+        {
+            Connection.OpenConnection();
+
+            Console.WriteLine(DBCommand);
+
+            Connection.CloseConnection();
+        }
+    }
+}

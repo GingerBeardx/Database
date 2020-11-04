a74b9587ef9feffc28fe02caac28b77652e7437a
GingerBeardx <ecryptographer@gmail.com>
2020-11-03 18:32:58 -0700

    Successfully ran both methods using polymorphism! Pretty awesome!

--- Database/Program.cs
+++ Database/Program.cs
@@ -6,7 +6,15 @@ namespace Database
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Database Simulator");
+            Console.WriteLine();
+
+            var connection = new OracleConnection("Connection to OracleDatabase");
+            var command = new DbCommand(connection, "Insert a new record into the DB");
+            command.Execute();
+
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
